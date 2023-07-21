0703dee27e09aa4c531c09c4608db5bb8859daa9
GregorJosh <grzegorz.jozw@gmail.com>
2023-07-21 02:53:10 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import api from './js/api';
 import { Loading, Notify } from 'notiflix';
+
+async function apiTest() {
+  const res = await api.get('search/keyword?query=monster');
+
+  console.log(res);
+}
+
+apiTest();

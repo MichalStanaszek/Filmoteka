2a515c9c1cba124a4cb3bfc231d5a9e352bd81f1
MichalStanaszek <michal.stanaszek@gmail.com>
2023-07-25 23:01:05 +0200

    add/modal window + bacis styles
    - create funcionality for open/close modal window
    - add close button (span) in modal-window.html
    - update component _backdrop.scss to center modal window on page
    - add some styles to modal window (will be updated later based on mixins/breakpoints)

--- src/js/movie-window.js
+++ src/js/movie-window.js
@@ -1,3 +1,32 @@
 import app from './global/app';
 
 app.apiTest('movie-window.js');
+
+const backdrop = document.querySelector('.backdrop');
+//const btn zamienić na const z kartą filmu jak będzie gotowa
+const btn = document.querySelector('#btn');
+const closeBtn = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+    backdrop.classList.toggle('hidden');
+  };
+  
+  // When the user clicks on <span> (x), close the modal
+  closeBtn.onclick = function () {
+    backdrop.classList.toggle('hidden');
+  };
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == backdrop) {
+      backdrop.classList.toggle('hidden');
+    }
+  };
+  
+  // When the user clicks on Escape key, close the modal
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      backdrop.classList.add('hidden');
+    }
+  });

8c9af55b5ff794c4bdb1f29cb27a993f1d38086f
GregorJosh <grzegorz.jozw@gmail.com>
2023-07-29 00:11:09 +0200

    add page param

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -2,7 +2,7 @@ import app from './global/app';
 
 const galleryULElement = document.getElementById(app.MOVIE_CARDS_PARENT_ELEMENT_ID);
 
-app.getMoviesTodayTrends = async function () {
+app.getMoviesTodayTrends = async function (page = app.currentPage) {
   return await app.api.get('trending/movie/day');
 };
 

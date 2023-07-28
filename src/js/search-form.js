ad905460ede32e45a91ad6b03b316dda55d14e3b
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-07-28 20:07:12 +0200

    create function onSearchFailed()

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -1,3 +1,25 @@
 import app from './global/app';
 
 app.apiTest('search-form.js');
+
+const formError = document.querySelector('.FormError');
+const formInput = document.querySelector('.FormInput');
+const formSearch = document.querySelector('.FormSearch');
+
+formError.style.display = 'none';
+
+onSearchFailed();
+
+function onSearchFailed() {
+  formSearch.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if (formInput.value.length === 0) {
+      formError.style.display = 'block';
+    } else {
+      formError.style.display = 'none';
+    }
+
+    return;
+  });
+}

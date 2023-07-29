68a5c24a7f2f079925dc4f7fc0500c8c4ef23be5
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-07-29 20:27:10 +0200

    keyword

--- src/js/keyword.js
+++ src/js/keyword.js
@@ -3,7 +3,8 @@ import app from './global/app';
 app.apiTest('keyword.js');
 
 app.getMoviesByKeyWord = async function (keyword) {
-  const keyword = await app.api.get('search/keyword?query=keyword');
+  app.currentKeyword = keyword;
+  const keyword = await app.api.get('search/keyword?query=${keyword}');
   console.log(keyword);
   return keyword;
 };

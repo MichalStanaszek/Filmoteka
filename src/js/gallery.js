f6326cea60e96e07ef848ae45bd29be7a38e12f5
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-07-30 22:41:59 +0200

    Update gallery.js

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,15 +1,10 @@
 import app from './global/app';
-
 app.getMoviesTodayTrends = async function (page = app.currentPage) {
   const galleryULElement = document.getElementById(
     app.MOVIE_CARDS_PARENT_ELEMENT_ID
   );
 
-<<<<<<< Updated upstream
-  const movies = await app.api.get('trending/movie/day', { page });
-=======
-  const movies = await app.api.get('movies/today/trending?page=${page}');
->>>>>>> Stashed changes
+  const movies = await app.api.get(`movie/popular?page=${page}`);
   const movieCards = await app.showMovieCards(movies);
 
   galleryULElement.insertAdjacentHTML('afterbegin', movieCards);

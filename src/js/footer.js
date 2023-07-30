daec89b6337ed6c9932d95518dc4da3be0eb8052
GregorJosh <grzegorz.jozw@gmail.com>
2023-07-30 19:51:19 +0200

    init

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,20 +1,25 @@
 import app from './global/app';
-const openAboutWindow = document.getElementById('about-us');
+const openAboutWindow = document.getElementById(app.ABOUT_LINK_A_ELEMENT_ID);
 const closeAboutWindow = document.getElementById('about-window-close');
-const backdropAboutWindow = document.getElementById('about-window-backdrop');
+const backdropAboutWindow = document.getElementById(app.ABOUT_WINDOW_BACKDROP_DIV_ELEMENT_ID);
 
 openAboutWindow.addEventListener('click', openModal);
 
 function closeModal() {
-  document.removeEventListener('keydown', escapeModal);
-  document.removeEventListener('click', closeModal);
+  window.removeEventListener('keydown', escapeModal);
+
+  closeAboutWindow.removeEventListener('click', closeModal);
+  backdropAboutWindow.removeEventListener('click', closeModal);
+
   backdropAboutWindow.classList.add('hidden');
 }
 function openModal() {
   backdropAboutWindow.classList.remove('hidden');
+
   closeAboutWindow.addEventListener('click', closeModal);
-  backdropAboutWindow.addEventListener('keydown', closeModal);
-  backdropAboutWindow.addEventListener('click', escapeModal);
+  backdropAboutWindow.addEventListener('click', closeModal);
+
+  window.addEventListener('keydown', escapeModal);
 }
 function escapeModal(ev) {
   if (ev.key === 'Escape' || ev.target === backdropAboutWindow) {

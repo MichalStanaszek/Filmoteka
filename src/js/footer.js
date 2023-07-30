64fe28cfabf98fc5fd6b6bc36f6c1d3ef6ed22b0
Samcia <samantta689@gmail.com>
2023-07-30 19:29:47 +0200

    bug fixes

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,2 +1,23 @@
 import app from './global/app';
+const openAboutWindow = document.getElementById('about-us');
+const closeAboutWindow = document.getElementById('about-window-close');
+const backdropAboutWindow = document.getElementById('about-window-backdrop');
 
+openAboutWindow.addEventListener('click', openModal);
+
+function closeModal() {
+  document.removeEventListener('keydown', escapeModal);
+  document.removeEventListener('click', closeModal);
+  backdropAboutWindow.classList.add('hidden');
+}
+function openModal() {
+  backdropAboutWindow.classList.remove('hidden');
+  closeAboutWindow.addEventListener('click', closeModal);
+  backdropAboutWindow.addEventListener('keydown', closeModal);
+  backdropAboutWindow.addEventListener('click', escapeModal);
+}
+function escapeModal(ev) {
+  if (ev.key === 'Escape' || ev.target === backdropAboutWindow) {
+    closeModal();
+  }
+}

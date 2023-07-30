1fe3dca769fd9d6388a267862d39b4e5bac3165b
Kayywu <katherine.wosik@gmail.com>
2023-07-30 18:02:28 +0200

    paginations.js

--- src/js/pagination-gallery.js
+++ src/js/pagination-gallery.js
@@ -1,1 +1,10 @@
 import app from './global/app';
+
+function activeLink() {
+    const buttons = document.querySelectorAll('.button');
+
+    buttons.forEach(button => button.classList.remove('active'))
+    event.target.classList.add('active')
+}
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener('click', activeLink));

1ee80dd7c934705056d103c34ae5f042d7f87e60
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-07-30 18:38:09 +0200

    keyword

--- src/js/keyword.js
+++ src/js/keyword.js
@@ -2,11 +2,26 @@ import app from './global/app';
 
 app.apiTest('keyword.js');
 
+// app.getMoviesByKeyWord = async function (keyword) {
+//   app.currentKeyword = keyword;
+//   const keyword = await app.api.get('search/keyword?query=${keyword}');
+//   console.log(keyword);
+//   return keyword;
+// };
+
+// app.getMoviesByKeyWord();
+
 app.getMoviesByKeyWord = async function (keyword) {
   app.currentKeyword = keyword;
-  const keyword = await app.api.get('search/keyword?query=${keyword}');
-  console.log(keyword);
-  return keyword;
+
+  const keywordData = await app.api.get(`search/keyword?query=${keyword}`);
+
+  if (keywordData.total_results > 0) {
+    return keywordData;
+  } else {
+    app.onSearchFailed();
+    throw new Error('No movies found');
+  }
 };
 
-app.getMoviesByKeyWord();
+app.getMoviesByKeyWord('keyword');

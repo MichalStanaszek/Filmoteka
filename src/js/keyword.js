61682c7ea79017374fe1e1fa7d2e4884d95fa597
GregorJosh <grzegorz.jozw@gmail.com>
2023-07-30 22:22:48 +0200

    init

--- src/js/keyword.js
+++ src/js/keyword.js
@@ -1,7 +1,5 @@
 import app from './global/app';
 
-app.apiTest('keyword.js');
-
 // app.getMoviesByKeyWord = async function (keyword) {
 //   app.currentKeyword = keyword;
 //   const keyword = await app.api.get('search/keyword?query=${keyword}');

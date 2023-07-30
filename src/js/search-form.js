1dc1bdd7fa54488157fe9356977eeb87da4ec7ef
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-07-30 16:53:22 +0200

    function repair

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -6,20 +6,16 @@ const formError = document.querySelector('.FormError');
 const formInput = document.querySelector('.FormInput');
 const formSearch = document.querySelector('.FormSearch');
 
-formError.style.display = 'none';
+formError.classList.add('hidden');
 
-onSearchFailed();
+app.onSearchFailed = function onSearchFailed(event) {
+  event.preventDefault();
 
-app.onSearchFailed = function onSearchFailed() {
-  formSearch.addEventListener('submit', event => {
-    event.preventDefault();
+  if (formInput.value.length === 0) {
+    formError.classList.remove('hidden');
+  } else {
+    formError.classList.add('hidden');
+  }
 
-    if (formInput.value.length === 0) {
-      formError.style.display = 'block';
-    } else {
-      formError.style.display = 'none';
-    }
-
-    return;
-  });
+  return;
 };

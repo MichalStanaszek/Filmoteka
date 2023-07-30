8c3cd2e06805270d435229fdf0eca3990dae2e2d
Łukasz Paradowski <123119119+Lukaszz2003@users.noreply.github.com>
2023-07-30 21:07:54 +0200

    keyword

--- src/js/keyword.js
+++ src/js/keyword.js
@@ -12,16 +12,36 @@ app.apiTest('keyword.js');
 // app.getMoviesByKeyWord();
 
 app.getMoviesByKeyWord = async function (keyword) {
-  app.currentKeyword = keyword;
-
-  const keywordData = await app.api.get(`search/keyword?query=${keyword}`);
-
-  if (keywordData.total_results > 0) {
-    return keywordData;
-  } else {
-    app.onSearchFailed();
-    throw new Error('No movies found');
+  if (env == 'dev') {
+    Loading.circle();
+    app.currentKeyword = keyword;
+    const keywordData = await app.api.get(
+      `search/keyword?query=${keyword}&page${app.currentPage}`
+    );
+    if (keywordData.total_results > 0) {
+      return keywordData;
+    } else {
+      app.onSearchFailed();
+      throw new Error('No movies found');
+    }
   }
+  setTimeout(function () {
+    Notify.success('Notiflix Test');
+    console.log('app.js');
+
+    Loading.remove();
+  }, 2000);
 };
+app.currentKeyword = keyword;
+
+//   const keywordData = await app.api.get(`search/keyword?query=${keyword}&page${app.currentPage}`);
+
+//   if (keywordData.total_results > 0) {
+//     return keywordData;
+//   } else {
+//     app.onSearchFailed();
+//     throw new Error('No movies found');
+//   }
+// };
 
 app.getMoviesByKeyWord('keyword');

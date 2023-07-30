8ad6b3ab8f2aec3036915ab0b8c4396918fd8957
GregorJosh <grzegorz.jozw@gmail.com>
2023-07-30 22:08:52 +0200

    init

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -1,19 +1,9 @@
 import app from './global/app';
 
 const formError = document.querySelector('.FormError');
-const formInput = document.querySelector('.FormInput');
-const formSearch = document.querySelector('.FormSearch');
 
 formError.classList.add('hidden');
 
-app.onSearchFailed = function onSearchFailed(event) {
-  event.preventDefault();
-
-  if (formInput.value.length === 0) {
-    formError.classList.remove('hidden');
-  } else {
-    formError.classList.add('hidden');
-  }
-
-  return;
+app.onSearchFailed = function () {
+  formError.classList.remove('hidden');
 };

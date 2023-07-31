ac7ce8fa64ef6007b433a2d957214951c1befd4f
GregorJosh <grzegorz.jozw@gmail.com>
2023-07-31 19:30:44 +0200

    init

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import app from './js/global/app';
 
 app.currentWebPage = 'home';
 
-console.log("Current Web Page: " + app.currentWebPage);
-
 import './js/gallery';
 import './js/keyword';
 import './js/search-form';
 import './js/pagination-gallery';
 import './js/footer';
 import './js/movie-window';
-
-console.log("Current Home Movies Page: " + app.currentPage);

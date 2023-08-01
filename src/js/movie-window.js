ac456dab4096af451729d0b14d6577cb69b31a8b
GregorJosh <grzegorz.jozw@gmail.com>
2023-08-01 19:00:05 +0200

    overall fixes

--- src/js/movie-window.js
+++ src/js/movie-window.js
@@ -1,78 +1,111 @@
 import app from './global/app';
 
-const backdrop = document.querySelector('.backdrop');
-const modalCard = document.querySelector('#gallery');
-const movieWindow = document.querySelector('.window');
+app.getMovieByID = async function (movieId) {
+  app.Loading.circle();
 
-const markup = `<span class="close">&#10006;</span>
-<div class="movie-window--flex">
-  <div>
-    <img
-      class="dummy"
-      src="/src/images/header/header-gallery/header-mobile.png"
-      alt="dummy-img"
-    />
-  </div>
-  <div>
-    <h1 class="dummy__title">MOVIE TITLE</h1>
-    <div class="stats">
-      <ul classs="stats__subtitles">
-        <li class="stats__list">Vote / Votes</li>
-        <li class="stats__list">Popularity</li>
-        <li class="stats__list">Original Title</li>
-        <li class="stats__list">Genre</li>
-      </ul>
-      <ul>
-        <li class="stats__result">
-          <span class="stats--color">7.3</span> /
-          <span class="stats--color2">1260</span>
-        </li>
-        <li class="stats__result"><span>100.2</span></li>
-        <li class="stats__result">A FISTFUL OF LEAD</li>
-        <li class="stats__result">Western</li>
-      </ul>
-    </div>
-    <section class="description">
-      <h2 class="description--header">ABOUT</h2>
-      <p>
-        Four of the West’s most infamous outlaws assemble to steal a huge
-        stash of gold from the most corrupt settlement of the gold rush
-        towns. But not all goes to plan one is killed and the other three
-        escapes with bags of gold hide out in the abandoned gold mine where
-        they happen across another gang of three – who themselves were
-        planning to hit the very same bank! As tensions rise, things go from
-        bad to worse as they realise the bags of gold are filled with
-        lead... they’ve been double crossed – but by who and how?
-      </p>
-    </section>
-    <div class="modal-buttons">
-      <button class="button" type="button">ADD TO WATCHED</button>
-      <button class="button" type="button">ADD TO QUEUE</button>
-    </div>
-  </div> 
-</div>`;
-movieWindow.insertAdjacentHTML('beforeend', markup);
+  const movie = await app.api.get('movie/' + movieId);
 
-const closeBtn = document.querySelector('.close');
+  app.Loading.remove();
+  return movie;
+};
 
-window.addEventListener('keydown', closeModal);
-backdrop.addEventListener('click', closeModal);
+const MOVIE_WINDOW_ID = 'movie-window';
 
-function toggleModal() {
-  backdrop.classList.toggle('hidden');
-}
+function onGalleryUlClick(event) {
+  const element = event.target;
 
-function openModal() {
-  modalCard.addEventListener('click', toggleModal);
-}
-openModal();
+  if (element.nodeName === 'LI') {
+    const movieId = app.getMovieIdFromMovieCardElement(element);
 
-function closeModal(event) {
-  if (event.key === 'Escape' || event.target === backdrop) {
-    toggleModal();
-  } else {
-    closeBtn.addEventListener('click', toggleModal);
+    app.getMovieByID(movieId).then(movie => {
+      createMovieWindow(movie);
+    });
   }
+}
+
+function onKeyPressed(event) {
+  if (event.key === "Escape") {
+    destroyMovieWindow();
+  }
+}
+
+function createMovieWindow(movieObject) {
+  const backdrop = document.getElementById(
+    app.MOVIE_WINDOW_BACKDROP_DIV_ELEMENT_ID
+  );
+  const movieWindow = document.getElementById(MOVIE_WINDOW_ID);
+
+  const moviePoster =
+    'https://image.tmdb.org/t/p/w500' + movieObject.poster_path;
+  const movieTitle = movieObject.original_title;
+  const movieVote = movieObject.vote_average;
+  const movieNumOfVotes = movieObject.vote_count;
+  const moviePopularity = movieObject.popularity;
+  const movieOrigTitle = movieObject.original_title;
+  const movieGenre = movieObject.genres[0].name;
+  const movieOverview = movieObject.overview;
+
+  const markup = `
+    <button id="close-btn" class="movie-window__close-btn button" type="button">&#9587;</button>
+    <div>
+      <img class="dummy" src="${moviePoster}" alt="dummy-img" />
+    </div>
+    <div>
+      <h1 class="dummy__title">${movieTitle}</h1>
+      <div class="stats">
+        <ul class="stats__subtitles">
+          <li class="stats__list">Vote / Votes</li>
+          <li class="stats__list">Popularity</li>
+          <li class="stats__list">Original Title</li>
+          <li class="stats__list">Genre</li>
+        </ul>
+        <ul>
+          <li class="stats__result">
+            <span class="stats--color">${movieVote}</span> /
+            <span class="stats--color2">${movieNumOfVotes}</span>
+          </li>
+          <li class="stats__result"><span>${moviePopularity}</span></li>
+          <li class="stats__result">${movieOrigTitle}</li>
+          <li class="stats__result">${movieGenre}</li>
+        </ul>
+      </div>
+      <div class="description">
+        <h2 class="description--header">ABOUT</h2>
+        <p>${movieOverview}</p>
+      </div>
+      <div class="modal-buttons">
+        <button class="button" type="button" data-movie="${movieObject.id}">ADD TO WATCHED</button>
+        <button class="button" type="button" data-movie="${movieObject.id}">ADD TO QUEUE</button>
+      </div>
+    </div>`;
+
+  movieWindow.insertAdjacentHTML('afterbegin', markup);
+
+  const closeBtn = document.getElementById('close-btn');
+
+  window.addEventListener('keydown', onKeyPressed);
+
+  backdrop.addEventListener('click', destroyMovieWindow);
+  closeBtn.addEventListener('click', destroyMovieWindow);
+
+  backdrop.classList.remove('hidden');
+}
+
+function destroyMovieWindow() {
+  const backdrop = document.getElementById(
+    app.MOVIE_WINDOW_BACKDROP_DIV_ELEMENT_ID
+  );
+  const movieWindow = document.getElementById(MOVIE_WINDOW_ID);
+  const closeBtn = document.getElementById('close-btn');
+
+  window.removeEventListener('keydown', destroyMovieWindow);
+
+  backdrop.removeEventListener('click', destroyMovieWindow);
+  closeBtn.removeEventListener('click', destroyMovieWindow);
+
+  movieWindow.innerHTML = '';
   backdrop.classList.add('hidden');
 }
-// window.removeEventListener('keydown', closeModal);
+
+const galleryDiv = document.getElementById(app.MOVIE_CARDS_PARENT_ELEMENT_ID);
+galleryDiv.addEventListener('click', onGalleryUlClick);
